e8bdd6b76af8afe30181bf568c2b1b8323e4ae6d
akariaka@yandex.ru <akariaka@yandex.ru>
2026-08-22 03:03:16 +0600

    socket: connect working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
 import Form from "./components/form/form";
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+// console.log(socket);
+
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+      socket.connect();
+      console.log(socket);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      console.log(isConnected);
+
+    }
+
+    function onFooEvent(value: any) {
+      setFooEvents(previous => [...previous, value]);
+      console.log(fooEvents);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+  // console.log(socket);
+
   return (
     <>
       <div className="w-[100%] h-[100vh] flex items-center justify-center">
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,55 @@
-console.log(1);
+import express, {type Request, type Response} from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+const port = 3000;
+const app = express();
+const httpServer = createServer(app);
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173/', // замените на ваш адрес фронтенда
+//   credentials: true, // если передаете куки или авторизационные заголовки
+// };
+
+
+app.use(cors());
+
+// const serverHttp = http.createServer(app);
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "http://localhost:5173/",
+//     // methods: ["GET", "POST"]
+//   }
+// })
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
+io.on("connection", (socket) => {
+  console.log(socket);
+})
+
+httpServer.listen(port)
+
+// const rooms: Map<string, string> = new Map();
+
+// io.listen(port);
+// app.use(express.json());
+
+// app.get('/rooms', (req, res) => {
+//   rooms.set('hello', '');
+//   res.json(rooms);
+// })
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<p>server working</p>');
+  console.log('path: /');
+})
+
+
+app.listen(port, () => {
+  console.log(`listen port ${port}`);
+})
